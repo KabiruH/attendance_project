c5ec91206e09e4128957bcf54e7276d91a275507
Nicco99 <nicholasnjeru917@gmail.com>
2025-01-21 23:14:43 +0300

    added profile page

---
+++ src/app/(dashboard)/profile/page.tsx
@@ -0,0 +1,8 @@
+
+const Profile = ()=>{
+return(<div>
+<p>Profile page</p>
+</div>)
+}
+
+export default Profile
--- src/lib/db-config.ts
+++ src/lib/db-config.ts
@@ -1,7 +1,7 @@
 //db-config.ts
 export const dbConfig = {
-    server: process.env.DB_SERVER || 'MARSHMELLO',
-    database: process.env.DB_NAME || 'ATTENDANCEDB',
+    server: process.env.DB_SERVER || 'NICCO99',
+    database: process.env.DB_NAME || 'attendance',
     options: {
       trustServerCertificate: true,
       trustedConnection: true, 
